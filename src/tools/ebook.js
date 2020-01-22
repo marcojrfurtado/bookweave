84139e482865977e3e782fd0ea0d331c9d87aeb8
Antonio Carlos Furtado <marcojrfurtado@gmail.com>
2020-01-21 22:36:08 -0500

    Remove nonsensical use of Promises within async functions

--- src/tools/ebook.js
+++ src/tools/ebook.js
@@ -15,96 +15,85 @@ const guessISBNInIdentifier = (identifier)  => {
     return result
 }
 
-const findFingerprint  = async(fingerprint) => new Promise(async(resolve, reject) => {
-    try {
-        const txids = await arweaveInstance.arql({
-            op: "and",
-            expr1: {
-                op: "equals",
-                expr1: "fingerprint",
-                expr2: fingerprint
-            },
-            expr2: queryWithinApp()
-        })
-        resolve(txids.length > 0)
-    } catch (error) {
-        reject(error)
-    }
-});
+const findFingerprint  = async(fingerprint) => {
 
-const addAppTagsToTransaction = async(transaction) => new Promise(async (resolve) => {
+    const txids = await arweaveInstance.arql({
+        op: "and",
+        expr1: {
+            op: "equals",
+            expr1: "fingerprint",
+            expr2: fingerprint
+        },
+        expr2: queryWithinApp()
+    })
+    return txids.length > 0
+}
+
+const addAppTagsToTransaction = async(transaction) => {
 
     await transaction.addTag('ArAppName', ArAppName)
     await transaction.addTag('ArAppVersion', ArAppVersion)
     await transaction.addTag('ArAppMode', ArAppMode)
 
-    resolve(transaction)
-});
+    return transaction
+}
 
-const loadMetadata = async(eBookName, rawEBookData) => new Promise(async (resolve, reject) => {
-    
-    try {
-        var doc = undefined
-        var inputMetadata = undefined
-        var fingerprint = undefined
+const loadMetadata = async(eBookName, rawEBookData) => {   
 
-        if (eBookName.endsWith(".epub")) {
-            doc = new Epub(rawEBookData, {});
-            inputMetadata = await doc.loaded.metadata
-            fingerprint = inputMetadata.identifier
-        } else {
-            reject("Unkown eBook format.")
-        }
+    var doc = undefined
+    var inputMetadata = undefined
+    var fingerprint = undefined
 
-        var outputMetadata = {}
-
-        outputMetadata['fingerprint'] = fingerprint
-        outputMetadata['author'] = inputMetadata.info ? inputMetadata.info.Author : inputMetadata.creator
-        outputMetadata['fileType'] = "epub"
-        outputMetadata['Content-Type'] = "application/epub+zip"
-        outputMetadata['title'] = inputMetadata.info ? inputMetadata.info.Title : inputMetadata.title
-        outputMetadata['searchTitle'] = createSearchPattern(outputMetadata.title)
-        outputMetadata['searchAuthor'] = createSearchPattern(outputMetadata.author)
-        outputMetadata['isbn'] = guessISBNInIdentifier(outputMetadata.fingerprint)
-
-        resolve(outputMetadata)
-    } catch(error) {
-        reject(error)
+    if (eBookName.endsWith(".epub")) {
+        doc = new Epub(rawEBookData, {});
+        inputMetadata = await doc.loaded.metadata
+        fingerprint = inputMetadata.identifier
+    } else {
+        throw Error("Unkown eBook format.")
     }
-});
+
+    var outputMetadata = {}
+
+    outputMetadata['fingerprint'] = fingerprint
+    outputMetadata['author'] = inputMetadata.info ? inputMetadata.info.Author : inputMetadata.creator
+    outputMetadata['fileType'] = "epub"
+    outputMetadata['Content-Type'] = "application/epub+zip"
+    outputMetadata['title'] = inputMetadata.info ? inputMetadata.info.Title : inputMetadata.title
+    outputMetadata['searchTitle'] = createSearchPattern(outputMetadata.title)
+    outputMetadata['searchAuthor'] = createSearchPattern(outputMetadata.author)
+    outputMetadata['isbn'] = guessISBNInIdentifier(outputMetadata.fingerprint)
+
+    return outputMetadata
+}
 
 
-const createTransationFromEBookData = async(eBookName, rawEBookData, arweaveWallet) => new Promise(async (resolve, reject) => {
-    try {
-        const data = Buffer.from(rawEBookData)
-        let transaction = await arweaveInstance.createTransaction({ data }, arweaveWallet)
+const createTransationFromEBookData = async(eBookName, rawEBookData, arweaveWallet) => {
+    const data = Buffer.from(rawEBookData)
+    let transaction = await arweaveInstance.createTransaction({ data }, arweaveWallet)
 
-        await addAppTagsToTransaction(transaction)
+    await addAppTagsToTransaction(transaction)
 
-        // NOTE: Future use
-        const totalParts = 1
-        await transaction.addTag('part', 0)
-        await transaction.addTag('totalParts', totalParts)
+    // NOTE: Future use
+    const totalParts = 1
+    await transaction.addTag('part', 0)
+    await transaction.addTag('totalParts', totalParts)
 
-        await transaction.addTag('sizeBytes', rawEBookData.byteLength)
+    await transaction.addTag('sizeBytes', rawEBookData.byteLength)
 
-        const metadata = await loadMetadata(eBookName, rawEBookData)
+    const metadata = await loadMetadata(eBookName, rawEBookData)
 
-        const foundFingerprint = await findFingerprint(metadata.fingerprint)
-        if (foundFingerprint) {
-            reject("A copy of this file has already been uploaded to the network.")
-        }
-
-        for (var key in metadata){
-            await transaction.addTag(key, metadata[key])
-        }
-
-        const fee = await arweaveInstance.ar.winstonToAr(transaction.reward)
-        resolve({transaction, fee, metadata})
-    } catch (error) {
-        reject(error)
+    const foundFingerprint = await findFingerprint(metadata.fingerprint)
+    if (foundFingerprint) {
+        throw Error("A copy of this file has already been uploaded to the network.")
     }
-});
+
+    for (var key in metadata){
+        await transaction.addTag(key, metadata[key])
+    }
+
+    const fee = await arweaveInstance.ar.winstonToAr(transaction.reward)
+    return {transaction, fee, metadata}
+}
 
 const queryWithinApp = () => {
     return {
@@ -130,82 +119,61 @@ const queryWithinApp = () => {
     }
 }
 
-const getAllTransactionsIds = async() => new Promise(async(resolve, reject) => {
-    try {
-        const txids = await arweaveInstance.arql(queryWithinApp())
-        resolve(txids)
-    } catch(error) {
-        reject(error)
-    }
-});
+const getAllTransactionsIds = async() => {
+    const txids = await arweaveInstance.arql(queryWithinApp())
+    return txids
+}
 
-const queryTransactionIds = async(searchField, searchValue) => new Promise(async(resolve, reject) => {
-    try {
-        const txids = await arweaveInstance.arql({
-            op: "and",
-            expr1: {
-                op: "equals",
-                expr1: searchField,
-                expr2: searchValue
-            },
-            expr2: queryWithinApp()})
-        resolve(txids)
-    } catch(error) {
-        reject(error)
-    }
-});
+const queryTransactionIds = async(searchField, searchValue) => {
+    const txids = await arweaveInstance.arql({
+        op: "and",
+        expr1: {
+            op: "equals",
+            expr1: searchField,
+            expr2: searchValue
+        },
+        expr2: queryWithinApp()})
+    return txids
+}
 
-const fetchTransactions = async(transactionIds) => new Promise(async(resolve, reject) => {
-    try {
-        var docInfoArray = []
-        for (var i = 0; i < transactionIds.length; i++) {
-            const txid = transactionIds[i]
+const fetchTransactions = async(transactionIds) => {
+    var docInfoArray = []
+    for (var i = 0; i < transactionIds.length; i++) {
+        const txid = transactionIds[i]
 
-            const transaction = await arweaveInstance.transactions.get(txid)
-            var docInfo = {}
-            const tags = transaction.get('tags')
-            for (var j = 0; j < tags.length; j++) {
-                const tag = tags[j]
-                let key = tag.get('name', {decode: true, string: true});
-                let value = tag.get('value', {decode: true, string: true});
-                docInfo[key] = value
-            }
-            docInfo['transaction_id'] = txid
-            docInfo['data'] = transaction.get('data', {decode: true, string: false})
-            docInfoArray.push(docInfo)
+        const transaction = await arweaveInstance.transactions.get(txid)
+        var docInfo = {}
+        const tags = transaction.get('tags')
+        for (var j = 0; j < tags.length; j++) {
+            const tag = tags[j]
+            let key = tag.get('name', {decode: true, string: true});
+            let value = tag.get('value', {decode: true, string: true});
+            docInfo[key] = value
         }
-        resolve(docInfoArray)
-    } catch (error) {
-        console.error(error)
-        reject(error)
+        docInfo['transaction_id'] = txid
+        docInfo['data'] = transaction.get('data', {decode: true, string: false})
+        docInfoArray.push(docInfo)
     }
-});
+    return docInfoArray
+}
 
 const shuffle = (array) => {
     array.sort(() => Math.random() - 0.5);
     return array
 }
 
-const getNDocs = async(n) => new Promise(async(resolve, reject) => {
-    try {
-        const txids = await getAllTransactionsIds()
-        const randomSubsetTxids = shuffle(txids).slice(0, n)
-        const docInfo = await fetchTransactions(randomSubsetTxids)
-        resolve(docInfo)
-    } catch(error) {
-        reject(error)
-    }
-});
+const getNDocs = async(n) => {
+    const txids = await getAllTransactionsIds()
+    const randomSubsetTxids = shuffle(txids).slice(0, n)
+    const docInfo = await fetchTransactions(randomSubsetTxids)
+    return docInfo
+}
 
-const queryDocs = async(searchField, searchValue) => new Promise(async(resolve, reject) => {
-    try {
-        const txids = await queryTransactionIds(searchField, searchValue)
-        const docInfo = await fetchTransactions(txids)
-        resolve(docInfo)
-    } catch(error) {
-        reject(error)
-    }
-});
+const queryDocs = async(searchField, searchValue) => {
+    const txids = await queryTransactionIds(searchField, searchValue)
+    const docInfo = await fetchTransactions(txids)
+    return docInfo
+}
 
 export{
     supportedExtensions,
